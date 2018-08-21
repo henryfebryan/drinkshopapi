d260b76a31ad282288c62615f7b7dc59197b54bf
henryfebryan <henry_febryan@yahoo.com>
2018-08-21 11:55:13 +0700

    init commit
    - register user
    - get user information
    - get category drink
    - get drink list
    - get banner image

---
+++ db_functions.php
@@ -0,0 +1,128 @@
+<?php
+
+class DB_Functions{
+
+	private $conn;
+
+	function __construct(){
+		require_once 'db_connect.php'; 
+		$db = new DB_Connect();
+		$this->conn = $db->connect();
+	}
+
+	function __destruct(){
+
+	}
+
+	/*
+		CheckUser exist
+	*/
+	function checkExistsUser($phone){
+		$stmt = $this->conn->prepare("SELECT * FROM user WHERE Phone=?");
+		$stmt->bind_param("s", $phone);
+		$stmt->execute();
+		$stmt->store_result();
+
+		if($stmt->num_rows > 0){
+			$stmt->close();
+			return true;
+		}else{
+			$stmt->close();
+			return false;
+		}
+	}
+
+	/*
+		Register new user
+		return User object if user was created
+		return false and show Error message if have exception
+	*/
+	public function registerNewUser ($phone, $name, $birthdate, $address) {
+		$stmt = $this->conn->prepare("INSERT INTO user(Phone,Name,Birthdate,Address) VALUES(?,?,?,?)");
+		$stmt->bind_param("ssss", $phone, $name, $birthdate, $address);
+		$result = $stmt->execute();
+		$stmt->close();
+
+		if($result){
+			$stmt = $this->conn->prepare("SELECT * FROM user WHERE Phone = ?");
+			$stmt->bind_param("s", $phone);
+			$stmt->execute();
+			$user = $stmt->get_result()->fetch_assoc();
+			$stmt->close();			
+			return $user;
+		}else{
+			return false;
+		}
+	}
+
+	/*
+		Get User Information
+		return User object if user exists
+		return NULL if user not exists
+	*/
+	
+	public function getUserInformation ($phone) {
+		$stmt = $this->conn->prepare("SELECT * FROM User WHERE Phone=?");
+		$stmt->bind_param("s",$phone);
+
+		if($stmt->execute()){
+			$user = $stmt->get_result()->fetch_assoc();
+			$stmt->close();
+
+			return $user;
+		}else{
+			return NULL;
+		}
+	}		
+
+	/*
+		Get Banner
+		return Banner list
+	*/
+	
+	public function getBanners() {
+		$result = $this->conn->query("SELECT * FROM Banner ORDER BY ID LIMIT 3");
+
+		$banners = array();
+
+		while($item = $result->fetch_assoc()){
+			$banners[] = $item;
+		}
+		return $banners;
+	}	
+
+	/*
+		Get Menu
+		return Menu list
+	*/
+	
+	public function getMenu() {
+		$result = $this->conn->query("SELECT * FROM Menu");
+
+		$menu = array();
+
+		while($item = $result->fetch_assoc()){
+			$menu[] = $item;
+		}
+		return $menu;
+	}	
+
+	/*
+		Get Drink base menu Id
+		return drink list
+	*/
+	
+	public function getDrinkByMenuID($menuId) {
+		$result = $this->conn->query("SELECT * FROM Drink WHERE MenuId = '".$menuId."'");
+
+		$drinks = array();
+
+		while($item = $result->fetch_assoc()){
+			$drinks[] = $item;
+		}
+		return $drinks;
+	}	
+
+}
+
+?>
---
+++ getbanner.php
@@ -0,0 +1,7 @@
+<?php 
+	require_once 'db_functions.php';
+	$db = new DB_Functions();
+
+	$banners = $db->getBanners();
+	echo json_encode($banners);
+?>
